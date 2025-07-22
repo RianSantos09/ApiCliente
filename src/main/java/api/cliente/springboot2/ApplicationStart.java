c52e4d176e48573ccf794e458fe1dafd6558a636
RianSantos09 <riansantosn45@gmail.com>
2025-07-22 18:03:41 -0300

    criando a date util

--- src/main/java/api/cliente/springboot2/ApplicationStart.java
+++ src/main/java/api/cliente/springboot2/ApplicationStart.java
@@ -1,12 +1,13 @@
-package api.cliente.springboot2.start;
+package api.cliente.springboot2;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "api.cliente.springboot2")
+@SpringBootApplication
+
 public class ApplicationStart {
     public static void  main(String[] args){
         SpringApplication.run(ApplicationStart.class, args);
--- src/main/java/api/cliente/springboot2/controller/UsuarioController.java
+++ src/main/java/api/cliente/springboot2/controller/UsuarioController.java
@@ -1,17 +1,28 @@
 package api.cliente.springboot2.controller;
 
 import api.cliente.springboot2.domain.Cliente;
+import api.cliente.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("usuario")
+@Log4j2
+@AllArgsConstructor
 public class UsuarioController {
+    @Autowired
+    private DateUtil dateUtil;
     @GetMapping(path = "list")
- public List<Cliente> list(){
-    return List.of(new Cliente( "rian"), new Cliente("jonatas"));
+    public List<Cliente> list(){
+       log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+       return List.of(new Cliente( "rian"), new Cliente("jonatas"));
  }
 }

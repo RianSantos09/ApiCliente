287fec5c6b097d8c5fd045cdee469bdfebc40f1c
RianSantos09 <riansantosn45@gmail.com>
2025-08-13 11:14:21 -0300

    validaçoes de campos

--- src/main/java/dto/UsuarioRequestDto.java
+++ src/main/java/dto/UsuarioRequestDto.java
@@ -1,10 +1,17 @@
 package dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UsuarioRequestDto {
 
+    @NotEmpty(message = "nome obrigatorio")
+    @NotNull(message = "nome obrigatorio")
     private String name;
+    @NotEmpty(message = "senha obrigatoria")
+    @NotNull(message = "senha obrigatoria")
     private String senha;
 }
